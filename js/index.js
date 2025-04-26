2b3315e5c388f83f8f96a8910a485405105067ab
Roman Sharapov <r.sharapov@stream-telecom.ru>
2025-04-26 17:31:30 +0300

    feat(burger): add burger menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
+const menu = document.querySelector(".nav__mobile");
+const burger = document.querySelector(".header__burger");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("nav__mobile--active");
+  burger.classList.toggle("header__burger--active");
+
+  if (menu.classList.contains("nav__mobile--active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
+});
+
 const swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
-    // type: "fraction",
     clickable: true,
     renderBullet: function (index, className) {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
